43bdf36ebf0e565c14d27d6847eb969d5d786751
Michelebuffa <michele.buffa.1977@gmail.com>
2020-01-29 20:17:58 +0100

    Update Blink_task.ino

--- example/02.advance_multitask/Blink_task.ino
+++ example/02.advance_multitask/Blink_task.ino
@@ -1,30 +1,30 @@
-/*****************************************************
- * 
- *  Blink_func 
- *
- *  task นี้จะทำหน้าที่ สั่งไฟกระพริบ 
- *  สามารถปรับกระพริบเร็วช้าได้ตามกำหนด
- *  
- *  หมายเหตุ
- *  ภายใน task ให้ใช้ DELAY() ตัวพิมพ์ใหญ่ทั้งหมด
- *  
- ****************************************************/
+/ *************************************************** ****
+  *
+  * Blink_func
+  *
+  * This task will perform Order flashing lights
+  * Can adjust flashing quickly, slowly as scheduled
+  *
+  * Note
+  * Within the task, use all capital letters DELAY ()
+  *
+  **************************************************** ** /
 
 #define BLINK_LED_PIN       2
 #define DEFAULT_BLINK_DELAY 1000
 
 // Task Share Variables Info
-// xBlink_Dalay   : สำหรับ task ภายนอกกำหนดความเร็วการกระพริบใน task นี้ได้
-
-void Blink_func(void* arg){  // Blink_task เรียกทำงานที่นี่
-  VOID SETUP(){                       // VOID SETUP() ใน task ใช้พิมพ์ใหญ่
+// xBlink_Dalay: For external tasks, you can set the blink speed in this task.
+  
+void Blink_func(void* arg){  // Blink_task launch here
+  VOID SETUP(){                       // VOID SETUP () in the task using uppercase
     pinMode(BLINK_LED_PIN,OUTPUT);
     if( xBlink_Delay <= 0) xBlink_Delay = DEFAULT_BLINK_DELAY;
   }
 
-  VOID LOOP() {                       // VOID LOOP() ใน task ใช้พิมพ์ใหญ่
+  VOID LOOP() {                       // VOID LOOP() in the task using uppercase
     digitalWrite(BLINK_LED_PIN,HIGH);
-    DELAY(xBlink_Delay);              // DELAY(..) ใน task ใช้พิมพ์ใหญ่
+    DELAY(xBlink_Delay);              // DELAY(..) in the task using uppercase
     digitalWrite(BLINK_LED_PIN,LOW);
     DELAY(xBlink_Delay);
   }
