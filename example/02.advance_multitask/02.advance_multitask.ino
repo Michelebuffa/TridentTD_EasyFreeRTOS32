d333c5da7117e15b7341e4435e799b6ce669cab9
Michelebuffa <michele.buffa.1977@gmail.com>
2020-01-29 19:58:46 +0100

    Update 02.advance_multitask.ino

--- example/02.advance_multitask/02.advance_multitask.ino
+++ example/02.advance_multitask/02.advance_multitask.ino
@@ -1,41 +1,41 @@
-/*****************************************************************
- *  ตัวอย่าง 
- *     - WiFi task  : สำหรับจัดการ WiFi 
- *     - DHT task   : สำหรับ อ่านค่า DHT Sensor อย่างเดียวไปเรื่อยๆ
- *     - Blink task : สำหรับไฟกระพริบ แล้วแต่จะสั่งใช้งาน
- *     - AsyncWebServer task : สำหรับ สร้าง WebServer แบบ Asynchonous ทำงาน 
- *                             ทุกครั้งที่มีการต่อไวไฟสำเร็จ
- *  
- *  หมายเหตุ
- *  ติดตั้ง Library ทั้ง 4 ให้เรียบร้อย
- *  library สำหรับ WebServer แบบ Asynchronous
- *     https://github.com/me-no-dev/AsyncTCP
- *     https://github.com/me-no-dev/ESPAsyncWebServer
- * 
- *  library สำหรับ DHT sensor
- *     https://github.com/adafruit/Adafruit_Sensor
- *     https://github.com/adafruit/DHT-sensor-library
+/ *******************************************************************
+ * Example
+ * - WiFi task: for managing WiFi
+ * - DHT task: for reading DHT Sensor values only continuously
+ * - Blink task: for flashing lights Depending on usage
+ * - AsyncWebServer task: For creating Asynchonous WebServer.
+ * Every time a Wifi connection is successful
  *
- ******************************************************************/
+ * Note
+ * Install all 4 libraries
+ * library for Asynchronous WebServer
+ * https://github.com/me-no-dev/AsyncTCP
+ * https://github.com/me-no-dev/ESPAsyncWebServer
+ *
+ * library for DHT sensor
+ * https://github.com/adafruit/Adafruit_Sensor
+ * https://github.com/adafruit/DHT-sensor-library
+ *
+ ******************************************************************** /
 
 #include <TridentTD_EasyFreeRTOS32.h>
 
-// ประกาศ ตัวแปร task และ function สำหรับ task
+// declare task and function variables for task
 TridentOS   DHT_task, Blink_task, WiFi_task, AsyncWebServer_task;
 void DHT_func(void*), Blink_func(void*), WiFi_func(void*), AsyncWebServer_func(void*);
 
-//----พื้นที่ส่วนนี้สำหรับประกาศตัวแปรที่ใช้แชร์ข้อมูลกันระหว่าง task ------
-int     xBlink_Delay;    // กำหนดความเร็วของไฟกระพริบ       (มาตรฐานให้ใช้ x นำ)
-float   xDHT_temp;       // อุณหภูมิ จาก DHT ค่าล่าสุดที่อ่านได้  (มาตรฐานให้ใช้ x นำ)
-float   xDHT_humid;      // ความชื้น จาก DHT ค่าล่าสุดที่อ่านได้  (มาตรฐานให้ใช้ x นำ)
+// ---- This area is for declaring variables to share data between tasks ------
+int     xBlink_Delay;    // set the speed of flashing lights (Standard, use x lead)
+float   xDHT_temp;       // Temperature from DHT, the latest value that can be read (Standard, use x lead)
+float   xDHT_humid;      // Moisture from DHT. The latest value that can be read. (Standard, use x lead)
 //---------------------------------------------------------
 
-void setup(){  // setup() หลักตัวพิมพ์เล็ก
+void setup(){  // setup () lowercase digits
   Serial.begin(115200); Serial.println();
     
   DHT_task.start( DHT_func );
   WiFi_task.start( WiFi_func );  
 }
 
-void loop(){}  // loop() หลักตัวพิมพ์เล็ก ไม่จำเป็นต้องใช้แต่ให้มีไว้เสมอ
+void loop(){}  // loop (), lowercase digits Not needed, but always have
 
