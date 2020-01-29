13aada7dbddb6dc7e581387ef45f1bf8c8848bec
Michelebuffa <michele.buffa.1977@gmail.com>
2020-01-29 20:42:04 +0100

    Update task5.ino

--- example/Multitask_Template/task5.ino
+++ example/Multitask_Template/task5.ino
@@ -1,20 +1,20 @@
-// รูปแบบมาตรฐานฟังกชั่น task สำหรับ TridentTD's Multitask
+/ *
+  * Note
+  * Within the task function, always use the uppercase DELAY (..) instead.
+  * /
+  
+// Standard format, task function for TridentTD's Multitask
 
-void task5_func(void*)  {   // ฟังกชั่นที่ task5 เรียกทำงาน  - void ใช้ตัวพิมพ์เล็ก
-  //----พื้นที่สำหรับประกาศตัวแปรที่ใช้ภายใน task นี้เท่านั้น----
+void task5_func (void *) {// function that task5 activates - void uses lowercase
+   // ---- Space for declaring variables used within this task only ----
 
-  //-----------------------------------------------
+   // -----------------------------------------------
 
-  VOID SETUP() {  // VOID SETUP() ใน task ให้ใช้ตัวพิมพ์ใหญ่
+   VOID SETUP () {// VOID SETUP () in the task, use capital letters
 
-  }
-  VOID LOOP() {   // VOID LOOP() ใน task ให้ใช้ตัวพิมพ์ใหญ่
+   }
+   VOID LOOP () {// VOID LOOP () in the task, use uppercase
 
-  }
+   }
 }
-
-/*
- * หมายเหตุ
- * ภายใน task function ให้ใช้้ DELAY(..)  ตัวพิมพ์ใหญ่แทนเสมอ
- */
 
